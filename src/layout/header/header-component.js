9487ea45ea46b273a51b90412b4ba67afd4d3723
EVRARD-DA CUNHA Nathan <nathan.evrarddacunha@proton.me>
2025-02-22 13:52:30 +0100

    feat(ui): make header responsive

--- src/layout/header/header-component.js
+++ src/layout/header/header-component.js
@@ -1,30 +1,86 @@
-const headerTemplate = document.createElement(`template`);
-
-headerTemplate.innerHTML = `
-<header>
-    <!-- DESIGN: Facilitate website navigation. -->
-    <nav>
-        <h1>WeatherAdvanced</h1>
-        <ul>
-            <li><a href="#">Overview</a></li>
-            <li><a href="#">Weather Platform</a></li>
-            <li><a href="#">About Us</a></li>
-        </ul>
-    </nav>
-</header>
-`;
-
 class Header extends HTMLElement {
+    static get observedAttributes() {
+        return ['style-file'];
+    }
+
     constructor() {
         super();
+        this.shadowDom = this.attachShadow({ mode: 'closed' });
     }
 
     connectedCallback() {
-        const shadowDom = this.attachShadow({ mode: `closed` });
-        shadowDom.appendChild(headerTemplate.content);
+        this.render();
+
+        const closeBurgerMenu =
+            this.shadowDom.getElementById('close-burger-menu');
+        if (closeBurgerMenu) {
+            closeBurgerMenu.addEventListener('click', () => {
+                this.hideBurgerMenu();
+            });
+        }
+
+        const openBurgerMenu =
+            this.shadowDom.getElementById('open-burger-menu');
+        if (openBurgerMenu) {
+            openBurgerMenu.addEventListener('click', () => {
+                this.openBurgerMenu();
+            });
+        }
+    }
+
+    attributeChangeCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
+    hideBurgerMenu() {
+        const openHeader = this.shadowDom.querySelector('.header--open');
+        openHeader.style.display = 'none';
+        const closeHeader = this.shadowDom.querySelector('.header');
+        closeHeader.style.display = 'block';
+    }
+
+    openBurgerMenu() {
+        const openHeader = this.shadowDom.querySelector('.header--open');
+        openHeader.style.display = 'block';
+        const closeHeader = this.shadowDom.querySelector('.header');
+        closeHeader.style.display = 'none';
+    }
+
+    render() {
+        const styleFile = this.getAttribute('style-file') || `No style file`;
+
+        const headerTemplate = document.createElement('template');
+        headerTemplate.innerHTML = `
+        <link rel="stylesheet" href="${styleFile}" />
+        <header class="header">
+            <!-- DESIGN: Facilitate website navigation. -->
+            <nav class="navigation">
+                <h1 class="navigation__title">WeatherAdvanced</h1>
+                <ul class="navigation__items">
+                    <li class="navigation__item"><a class="navigation__link" href="#">Overview</a></li>
+                    <li class="navigation__item"><a class="navigation__link" href="#">Playground</a></li>
+                    <li class="navigation__item"><a class="navigation__link" href="#">Company</a></li>
+                    <img class="navigation__item header__button" id="open-burger-menu" src="assets/icons/icon-menu.svg" alt="open button for closed burger menu" />
+                </ul>
+            </nav>
+        </header>
+
+        <header class="header--open">
+            <nav class="navigation--open">
+                <h1 class="navigation__title">WeatherAdvanced</h1>
+                <ul class="navigation__items--open">
+                    <img class="header__button" id="close-burger-menu" src="assets/icons/icon-close.svg" alt="close button for opened burger menu" />
+                    <li><a class="navigation__link" href="#">Overview</a></li>
+                    <li><a class="navigation__link" href="#">Playground</a></li>
+                    <li><a class="navigation__link" href="#">Company</a></li>
+                </ul>
+            </nav>
+        </header>
+        `;
+
+        this.shadowDom.innerHTML = '';
+        this.shadowDom.appendChild(headerTemplate.content.cloneNode(true));
     }
 }
 
-// NOTE: Might need to make it dynamically receive inputs for the call-to-action inside it.
-// NOTE: If made dynamic, I will need to input the same inputs on all page with header defeating it's purpose.
 customElements.define('header-component', Header);
